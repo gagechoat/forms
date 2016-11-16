55e26edad480a4e10204c38b7bad75baef0bbc12
Gage Choat <gagechoat@gmail.com>
2016-11-16 10:18:07 -0800

    finish lesson 113

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -3,10 +3,28 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles: [`
+    input.ng-invalid {
+      border: 1px solid #d99;
+    }
+  `]
 })
 export class TemplateDrivenComponent {
+  user = {
+    username: 'Jimi',
+    email: 'jimi@hendrix.com',
+    password: '12345',
+    gender: 'other'
+  };
+
+  genders = [
+    'male',
+    'female',
+    'other'
+  ];
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(form.value);
   }
 }
